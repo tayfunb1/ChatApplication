0ede8e12faf7ef304235cb33d4f3845a4ae761ad
Tayfun <t.bakir@amygdalab.net>
2024-08-28 10:34:11 +0300

    commit to untrack

--- ChatApplication.Api/obj/Debug/net8.0/ChatApplication.Api.AssemblyInfo.cs
+++ ChatApplication.Api/obj/Debug/net8.0/ChatApplication.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApplication.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59d5c2fb9270e44c1cfad810cac69060b9a4ac89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4682c87edfaba1ba121855909b8983ac4328f24")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApplication.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApplication.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
